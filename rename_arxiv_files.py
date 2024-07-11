a4ac7a7b7a63e797d7bfd4066aafdaf96b13e19d
Gustavo Führ <gustavofuhr87@gmail.com>
2024-07-11 14:50:30 -0300

    UPD: add option to include year

--- rename_arxiv_files.py
+++ rename_arxiv_files.py
@@ -1,13 +1,12 @@
 import os
 import re
-import json
 import argparse
 
 import feedparser
 
 
 def get_arxiv_number(s):
-    p = re.compile("(\d{4}.\d{5})(v\d)?\.pdf")
+    p = re.compile(r"(\d{4}.\d{5})(v\d)?\.pdf")
     result = p.search(s)
     if result is not None:
         return result.group(1)
@@ -21,7 +20,7 @@ def fetch_paper_details_arxiv(arxiv_number):
 def sanitize_title(title):
     return title.replace(":", ",")
 
-def rename_arxiv_files(root_path, dry_run = False):
+def rename_arxiv_files(root_path, include_updated_year = False, dry_run = False):
     arxiv_files = {}
     pdf_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(root_path) \
                for f in filenames if os.path.splitext(f)[1].lower() == '.pdf']
@@ -35,9 +34,15 @@ def rename_arxiv_files(root_path, dry_run = False):
             arxiv_files[arxiv_no] = f_path
             
             arxiv_d = fetch_paper_details_arxiv(arxiv_no)
+            
             if "entries" in arxiv_d.keys() and len(arxiv_d["entries"]) > 0:
                 arxiv_title = arxiv_d["entries"][0]["title"]
-                new_filename = f"{arxiv_no} - {sanitize_title(arxiv_title)}.pdf"
+                if include_updated_year:
+                    # extract year from string like '2024-01-05T13:16:25Z'
+                    year_arxiv_updated = arxiv_d["entries"][0]["updated"][:4]
+                    new_filename = f"{arxiv_no} - {year_arxiv_updated} - {sanitize_title(arxiv_title)}.pdf"
+                else:
+                    new_filename = f"{arxiv_no} - {sanitize_title(arxiv_title)}.pdf"
                 new_filepath = f_path.replace(filename, new_filename)
                 cmd = f'mv "{f_path}" "{new_filepath}"'
                 print(cmd)
@@ -47,10 +52,10 @@ def rename_arxiv_files(root_path, dry_run = False):
 
 
 if __name__ == "__main__":
-    # read arguments from user
     parse = argparse.ArgumentParser("Rename arxiv PDF files recursively.")
     parse.add_argument("root_path", type=str, help="Path to the PDF folder.")
-    parse.add_argument("--dry_run", action="store_true", help="Just show which files will rename and to what.")
+    parse.add_argument("--dry-run", action="store_true", help="Just show which files will rename and to what.")
+    parse.add_argument("--include-year", action="store_true", help="Includes in the file name the year of last update.")
 
     args = parse.parse_args()
-    rename_arxiv_files(args.root_path, args.dry_run)
+    rename_arxiv_files(args.root_path, args.include_year, args.dry_run)
